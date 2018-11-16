c8ab25f9e9300d5b85d1d9c9bb10bf8bbc3f1427
Daniil Z <zubkov_dn@mail.ru>
2018-11-17 02:08:53 +0300

    Запись на мероприятие по ссылке с помощью Selenium

---
+++ open_city_selen.py
@@ -0,0 +1,63 @@
+﻿# Запись на мероприятие на открытыйгород.рф с помощью Selenium
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
+def read_login_data(file):
+    """Чтение логина и пароля из файла"""
+    with open(file) as f:
+        username = f.readline().strip()
+        password = f.readline().strip()
+    return username, password
+
+
+def set_driver():
+    """Создание драйвера"""
+    chrome_options = Options()
+    chrome_options.add_argument("--headless") # Браузер без gui
+    driver = webdriver.Chrome(
+        executable_path=r'C:\Py files\web-scraping\chromedriver.exe',
+        options=chrome_options)
+    return driver
+
+
+def login(driver, username, password):
+    """Авторизация"""
+    login_url = 'https://xn--c1acndtdamdoc1ib.xn--p1ai/vxod.html'
+    driver.get(login_url)
+    form_username = driver.find_element_by_name('username')
+    form_password = driver.find_element_by_name('password')
+    form_username.send_keys(username)
+    form_password.send_keys(password)
+    driver.find_element_by_name('Login').click()
+
+
+def try_to_enroll(driver, wanted_event_url):
+    """Попытка записи на мероприятие"""
+    driver.get(wanted_event_url)
+    # Ждать, пока не обнаружена нужная кнопка или не вышло время (15 сек)
+    try:
+        enroll_button = WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+            (By.XPATH, '//button[contains(text(), "Записаться")]')))
+        if enroll_button.text == 'ЗАПИСАТЬСЯ': # Исключить текст 'ЗАПИСАТЬСЯ НЕЛЬЗЯ'
+            enroll_button.click()
+            return 'Успешно'
+    except TimeoutException:
+        return 'Активная кнопка не обнаружена'
+
+
+def main():
+    wanted_event_url = 'https://xn--c1acndtdamdoc1ib.xn--p1ai/ekskursii/avangard-za-narvskoj-zastavoj.html?date=2018-11-23%2011:00:00'
+    username, password = read_login_data('login_data.txt')
+    driver = set_driver()
+    login(driver, username, password)
+    print(try_to_enroll(driver, wanted_event_url))
+
+
+if __name__ == '__main__':
+    main()
