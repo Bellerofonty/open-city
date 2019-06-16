04245200af543a57eef77c7f4a4d8dd9fe99e837
Daniil Z <zubkov_dn@mail.ru>
2019-06-17 01:07:31 +0300

    fix: не было импортировано исключение

--- open_city_selen.py
+++ open_city_selen.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 
 
 def read_from_file(file):
